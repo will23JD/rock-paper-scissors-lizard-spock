8fde3fcad178c72aaabebbae8f8151aae052178f
will23JD <will_jd@outlook.com>
2021-10-26 17:31:07 +0000

    Add how to play using html, css and javascript and a playable video

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,34 @@
-console.log('hello')
+let openModalButtons = document.querySelectorAll('[data-modal-target]')
+let closeModalButtons = document.querySelectorAll('[data-close-button]')
+let overlay = document.getElementById('overlay')
+
+openModalButtons.forEach(button => {
+  button.addEventListener('click',function () {
+    const modal = document.querySelector(button.dataset.modalTarget)
+    openModal(modal)
+  })
+})
+
+overlay.addEventListener('click', function(){
+  const modals = document.querySelectorAll('.help.active')
+  modals.forEach(modal => {
+    closeModal(modal)
+  })
+})
+
+closeModalButtons.forEach(button => {
+  button.addEventListener('click', function() {
+    const modal = button.closest('.help')
+    closeModal(modal)
+  })
+})
+
+function openModal(modal) {
+  modal.classList.add('active')
+  overlay.classList.add('active')
+}
+
+function closeModal(modal) {
+  modal.classList.remove('active')
+  overlay.classList.remove('active')
+}
