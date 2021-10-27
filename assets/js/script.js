2a8d732450f68dacd65d254e835b717e384ce8e1
will23JD <will_jd@outlook.com>
2021-10-27 18:06:45 +0000

    Add lizard and spock to js functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -38,13 +38,15 @@ function closeModal(modal) {
 let rock = document.getElementById('rock');
 let paper = document.getElementById('paper');
 let scissors = document.getElementById('scissors');
+let lizard = document.getElementById('lizard');
+let spock = document.getElementById('spock');
 let userScore = document.getElementById('user-score');
 let computerScore = document.getElementById('computer-score');
 let result = document.getElementById('result-text')
 
 function getComputerChoice() {
-  let options = ['rock', 'paper', 'scissors'];
-  let randomNum = (Math.floor(Math.random() * 3));
+  let options = ['rock', 'paper', 'scissors', 'lizard', 'spock'];
+  let randomNum = (Math.floor(Math.random() * 5));
   return options[randomNum];
 }
 
@@ -55,18 +57,34 @@ function choice(userChoice) {
     case 'rockscissors':
     case 'paperrock':
     case 'scissorspaper':
+    case 'rocklizard':
+    case 'lizardspock':
+    case 'spockscissors':
+    case 'scissorslizard':
+    case 'lizardpaper':
+    case 'paperspock':
+    case 'spockrock':
       ++userScore.innerHTML;
       result.innerHTML = `${userChoice} beats ${computerChoice}. You Win!`
       break;
     case 'scissorsrock':
     case 'rockpaper':
     case 'paperscissors':
+    case 'lizardrock':
+    case 'spocklizard':
+    case 'scissorsspock':
+    case 'lizardscissors':
+    case 'paperlizard':
+    case 'spockpaper':
+    case 'rockspock':
       ++computerScore.innerHTML;
       result.innerHTML = `${userChoice} loses to ${computerChoice}. You Lose.`
       break;
     case 'rockrock':
     case 'paperpaper':
     case 'scissorsscissors':
+    case 'lizardlizard':
+    case 'spockspock':
       result.innerHTML = `It's a Draw ${userChoice} = ${computerChoice}`
   }
     
@@ -83,6 +101,14 @@ function getUserChoice() {
 
   scissors.addEventListener('click', function() {
     choice('scissors');
+  })
+
+  lizard.addEventListener('click', function() {
+    choice('lizard');
+  })
+
+  spock.addEventListener('click', function() {
+    choice('spock');
   })
 }
 
